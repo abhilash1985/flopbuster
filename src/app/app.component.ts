dfd2047972db8e7154cbf6306c480f4b8ce96b76
Abhilash V <abhilash.vannarath@navigantbpm.com>
2021-03-29 00:40:45 +0530

    Vote App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'flopbuster';
+  // title = 'flopbuster';
+  movieTitle = 'Games';
+  bestMovieEver = false;
+
+  // Add Data Elements
+  favFlop = {
+  	title: 'Batman Begins',
+  	yearReleased: 2002,
+  	rating: 'PG-13',
+  	imdbLink: 'http://imbd.com/2312',
+  	soundTrack: {
+      spotifyLink: 'http://imbd.com/2312'
+  	}
+  }
+
+  onCheckout(event: any) {
+    console.log('Checkout', event.target);
+  }
+
+  titleHover(message: string) {
+    console.log(message + " hovered");
+  }
+
+  yearClicked(event: any) {
+    console.log("Year Clicked: " + event.target.innertHtml);
+    console.log("Year Clicked: " + event.target.innerHTML);
+  }
+
+  ratingClicked(event: any) {
+    console.log("Rating Clicked: "+ event.target.innerHTML);
+  }
+
+  selectedIndex: number = -1;
+
+  onVoteClick(index: number) {
+    if(this.isSelected(index)) {
+      this.selectedIndex = -1;
+    } else {
+      this.selectedIndex = index;
+    }
+  }
+
+  isSelected(index: number) {
+    return this.selectedIndex === index;
+  }
+
+  anySelected() {
+    return this.selectedIndex !== -1;
+  }
+
+  getButtonTextFor(index: number) {
+    if(this.selectedIndex === index) {
+      return 'UnVote';
+    } else {
+      return 'Vote';
+    }
+  }
 }
